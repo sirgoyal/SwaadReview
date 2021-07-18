6958fdc9979d7989faaea2e33153de3dbbc951c2
Shreeram Goyal <shriramgoyal31@gmail.com>
2021-07-18 15:00:31 +0530

    Added review model, create, delete and validation

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review')
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -6,6 +7,23 @@ const CampgroundSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 });
+//deletion middleware for deleting reviews from review model if a campground is deleted.
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc.reviews.length) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
 module.exports = mongoose.model('Campground', CampgroundSchema);
