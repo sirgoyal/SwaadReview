0cdb810be442c58632215e3bf175e1b2f5d2e803
Shreeram Goyal <shriramgoyal31@gmail.com>
2021-07-23 17:25:46 +0530

    Finished image upload and thumbnail virtual

--- schemas.js
+++ schemas.js
@@ -5,12 +5,14 @@ module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required(),
         price: Joi.number().required().min(0),
-        image: Joi.string().required(),
+        // image: Joi.string().required(),
         location: Joi.string().required(),
         description: Joi.string().required()
-    }).required()
+    }).required(),
+    deleteImages: Joi.array()
 });
-module.exports.reviewSchema= Joi.object({
+
+module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
         body: Joi.string().required()
