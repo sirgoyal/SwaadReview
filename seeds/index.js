b68dc0a02f223574947d6d14d0a5fe5980b1d395
Shreeram Goyal <shriramgoyal31@gmail.com>
2021-07-25 22:53:42 +0530

    Added cluster map to index

--- seeds/index.js
+++ seeds/index.js
@@ -14,7 +14,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {useNewUrlParser: true, 
 //fxn to create a db with random elements from both cities and seedHelpers file
 const seedDB = async () => {
     await Campground.deleteMany({}); // deletes prev stored camps
-    for (let i = 0; i < 50; i++) { // loop to access random cities from cities file
+    for (let i = 0; i < 300; i++) { // loop to access random cities from cities file
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({ //creating a new camp
@@ -23,6 +23,7 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
+            geometry: { coordinates: [ cities[random1000].longitude, cities[random1000].latitude ], type: 'Point' },
             images: [
                 {
                   url: 'https://res.cloudinary.com/sirgoyal/image/upload/v1627030922/yelpcamp/wiafkwralkqigq2at7wy.jpg',
