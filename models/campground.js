89110e77660568e4f853c9143263e9af4610cd8a
Shreeram Goyal <shriramgoyal31@gmail.com>
2021-07-25 22:55:31 +0530

    Added cluster map to index

--- models/campground.js
+++ models/campground.js
@@ -11,11 +11,22 @@ ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 });
 
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
     title: String,
-    images:[
-    ImageSchema    
-    ],
+    images: [ImageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
     price: Number,
     description: String,
     location: String,
@@ -29,7 +40,15 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+}, opts);
+
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
+
 //deletion middleware for deleting reviews from review model if a campground is deleted.
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc.reviews.length) {
