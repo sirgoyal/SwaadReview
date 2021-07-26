f15d7a17bd7a4230603452da65ff67939ae8d3a0
Shreeram Goyal <shriramgoyal31@gmail.com>
2021-07-27 00:10:22 +0530

    Added image upload to edit

--- middleware.js
+++ middleware.js
@@ -30,7 +30,7 @@ module.exports.isAuthor = async (req, res, next) => {
     const campground = await Campground.findById(id);
     if (!campground.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!');
-        return res.redirect(`/campgrounds/${id}`);
+        return res.redirect(`/foodOutlets/${id}`);
     }
     next();
 }
@@ -40,7 +40,7 @@ module.exports.isReviewAuthor = async (req, res, next) => {
     const review = await Review.findById(reviewId);
     if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!');
-        return res.redirect(`/campgrounds/${id}`);
+        return res.redirect(`/foodOutlets/${id}`);
     }
     next();
 }
