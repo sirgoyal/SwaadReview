d5a03114dd13ca6fa28e47d13e3b7a83b70e7846
Shreeram Goyal <shriramgoyal31@gmail.com>
2021-09-09 18:20:05 +0530

    Added edit review feature

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -13,6 +13,24 @@ module.exports.createReview = async (req, res) => {
     res.redirect(`/foodOutlets/${campground._id}`);
 }
 
+module.exports.renderEditForm = async (req, res) => {
+    const { id, reviewId } = req.params;
+    const campground = await Campground.findById(id);
+    const review = await Review.findById(reviewId);
+    if (!review) {
+        req.flash('error', 'Cannot find that review!');
+        return res.redirect('/foodOutlets');
+    }
+    res.render('reviews/edit', {campground, review});
+}
+
+module.exports.updateReview = async (req, res) => {
+
+    const { id, reviewId } = req.params;
+    const review = await Review.findByIdAndUpdate(reviewId, req.body.review);
+    req.flash('success', 'Successfully updated review!');
+    res.redirect(`/foodOutlets/${id}`);
+}
 module.exports.deleteReview = async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
--- routes/reviews.js
+++ routes/reviews.js
@@ -7,7 +7,9 @@ const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 
 router.post('/',  isLoggedIn, validateReview, catchAsync(reviews.createReview))
-
+router.get('/:reviewId/edit', isLoggedIn, catchAsync(reviews.renderEditForm))
+ 
+router.put('/:reviewId', isLoggedIn, catchAsync(reviews.updateReview))
 router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports = router;
